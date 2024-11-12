eca9987ef1b4ea87dad051e62d0b1de0a48a9925
Chris <cpitaz108@gmail.com>
2024-11-12 10:15:04 -0800

    Added getComputerChoice function to simulate rock, paper, or scissors

--- rps.js
+++ rps.js
@@ -1,1 +1,21 @@
-document.getElementById("rps-container").textContent = "RPS Works"
+//document.getElementById("rps-container").textContent = "RPS Works"
+
+//Randomly return Rock, Paper, or Scissors
+function getComputerChoice(){
+  //Gets a random number between 0, 1, and 2
+  const choice = Math.floor(Math.random() * 3);
+  //Switch statement based on the result, returns the raw string depending on choice
+  switch(choice){
+    case 0:
+      return "Rock"
+      break;
+    case 1: 
+      return "Paper"
+      break;
+    case 2:
+      return "Scissors"
+      break
+    default:
+      return "N/A"
+  }
+}

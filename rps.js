3fa83945d2c4a2f7e90484452a24aa06c1c32962
Chris <cpitaz108@gmail.com>
2024-11-12 10:23:13 -0800

    Added the getHumanChoice function to receive user input in the game

--- rps.js
+++ rps.js
@@ -4,18 +4,39 @@
 function getComputerChoice(){
   //Gets a random number between 0, 1, and 2
   const choice = Math.floor(Math.random() * 3);
-  //Switch statement based on the result, returns the raw string depending on choice
+  //Switch statement based on the result, returns the raw string depending on choice.
+  
+  /*Made the values all upper case to utilize the toUpperCase method rather than writing a slicing/indexing function
+  to capitalize the first letter only. I think this would also make the choice stand out more during a game*/
   switch(choice){
     case 0:
-      return "Rock"
+      return "ROCK"
       break;
     case 1: 
-      return "Paper"
+      return "PAPER"
       break;
     case 2:
-      return "Scissors"
+      return "SCISSORS"
       break
     default:
       return "N/A"
   }
+}
+
+//Gets the human choice for rock, paper, or scissors
+function getHumanChoice(){
+  //Sets empty variable for the result and an acceptable options array 
+  let choice = ""
+  const options = ["ROCK", "PAPER", "SCISSORS"]
+  /*Checks if the choice has been made, or if the choice didn't exist in the options array and returns
+    specific messages to the user depending on the situation*/
+  while(choice == "" || !options.includes(choice.toUpperCase())){
+    if(choice != ""){
+      choice = prompt("Check your spelling and try again.\nEnter Rock, Paper, or Scissors");
+    }else{
+      choice = prompt("Enter Rock, Paper, or Scissors")
+    }
+  }
+  //Returns the accepted value in upper case
+  return choice.toUpperCase();
 }
